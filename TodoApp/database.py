451eeaea924e83a6fe67282e0236b70489805ee2
Denis Dunca <denisa.dunca@qinsoft.ro>
2025-04-17 15:57:09 +0300

    change db

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:root@localhost:3306/fastapi_db'
+#SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://admin:root@localhost:3306/fastapi_db'
+SQLALCHEMY_DATABASE_URI = "sqlite:///./testdb.db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URI)
 
